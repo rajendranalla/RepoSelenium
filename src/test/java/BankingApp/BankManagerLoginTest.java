885cca7df5647f1d97811c057e04ea9b76fa5861
rajendra <rajendranalla@gmail.com>
2018-08-23 18:09:03 -0500

    23Aug2018

--- src/test/java/BankingApp/BankManagerLoginTest.java
+++ src/test/java/BankingApp/BankManagerLoginTest.java
@@ -10,10 +10,10 @@ public class BankManagerLoginTest extends TestBase {
 	@Test
 	public void loginAsBankManager() throws Exception {
 
-		Assert.assertTrue(isElementEnabled("bmlBtn_css"), "Bank manager button found");
-		click("bmlBtn_css");
+		Assert.assertTrue(isElementEnabled("bmlBtn_xpath"), "Bank manager button found");
+		click("bmlBtn_xpath");
 
-		Assert.assertTrue(isElementEnabled("addCustBtn_css"), "Add Customer button Enabled");
+		Assert.assertTrue(isElementEnabled("addCustBtn_xpath"), "Add Customer button Enabled");
 
 		// To fail the testcase
 		// Assert.fail("Bank Manager login not successful");
